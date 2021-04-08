9f226252d6b35db1179819cd332c214b35927eef
Killian Meersman <hi@killianm.dev>
2021-04-08 22:49:24 +0200

    Version 3.0.3

--- release.sh
+++ release.sh
@@ -2,6 +2,7 @@
 
 new_version="$1"
 old_version="$(git tag | grep -E 'v.*\..*\..*' | sort -V | tail -n 1 | sed -E 's/v(.*)/\1/')"
+version_file=setup.py
 
 if [[ $(git branch --show-current) != 'master' ]]; then
     echo 'Not on dev branch'
@@ -11,17 +12,21 @@ fi
 echo "Upgrading from $old_version to $new_version"
 
 upload_pypi() {
+    rm -r dist
     python setup.py sdist bdist_wheel
     python3 -m twine upload --repository pypi dist/*
 }
 
-# Change source version
-sed -i -E "s/version=\"$old_version\"/version =\"$new_version\"/" setup.py
+change_version() {
+    sed -i -E "s/version=\"$1\"/version =\"$2\"/" $version_file
+}
 
-# Create git tag
+# Change source version
+change_version $old_version $new_version
+
+# Create git commit and tag
 git commit -a -m "Version $new_version"
 git tag -a "v$new_version" -m "Version $new_version"
-
 
 # Confirm before pushing
 read -p "Creating new release $new_version, finalize? (y/N)" -n 1 -r
@@ -33,6 +38,6 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo 'Release finalized'
 else
     git tag -d "v$new_version"
-    sed -i -E "s/version = \"$new_version\"/version = \"$old_version\"/" pkg/config/config.go
+    change_version $new_version $old_version
     echo 'Aborted'
 fi
